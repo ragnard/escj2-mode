1dad26786d03986e8c91977e1a97854612e7f890
Ragnar Dahlén <r.dahlen@gmail.com>
2009-02-16 08:23:26 +0100

    Add functionality to verify current method.

--- escj2-mode.el
+++ escj2-mode.el
@@ -1,7 +1,14 @@
 ;;;
+
+;;;;;;; escj2-java: ESC/Java2 Minor Mode
+;;;;;;; Version 0.1
+
+;;; Author: Ragnar Dahlén (r.dahlen@gmail.com)
 ;;;
 ;;;
-;;;
+
+(require 'jde)
+
 (defvar escj2-command nil
   "Command for ESC/Java2 executable")
 
@@ -14,6 +21,7 @@
 (defvar escj2-output-buffer-name nil
   "Name of output buffer for ESC/Java2") 
 
+;;; Set default values
 (progn
   (setq escj2-command "escj"))
 
@@ -22,12 +30,14 @@
 
 (progn
   (setq escj2-mode-keymap (make-sparse-keymap))
+  (define-key escj2-mode-keymap (kbd "C-c m") 'escj2-verify-method-at-point)
   (define-key escj2-mode-keymap (kbd "C-c v") 'escj2-verify-current-buffer)
   (define-key escj2-mode-keymap (kbd "C-c b v") 'escj2-verify-buffer))
 
 (progn
   (setq escj2-output-buffer-name "*escj2-output*"))
 
+;;; Define Minor Mode
 (define-minor-mode escj2-mode
   "Toggle ESC/Java2 mode.
      With no argument, this command toggles the mode.  
@@ -38,15 +48,49 @@
   escj2-mode-keymap
   :group 'escj2)
 
+
 (defun escj2-verify-current-buffer ()
+  "Verify the current buffer"
   (interactive)
   (escj2-verify-buffer (buffer-name (current-buffer))))
 
+
 (defun escj2-verify-buffer (buffer)
+  "Verify the contents of a buffer"
   (interactive "bBuffer: ")
   (let ((file-name (buffer-file-name (get-buffer buffer))))
-    (switch-to-buffer-other-window escj2-output-buffer-name)
-    (goto-char (point-min))
-    ;(erase-buffer)
-    (start-process "escj" escj2-output-buffer-name escj2-command file-name escj2-command-options)))
+    (escj2-run-verifier file-name escj2-command-options)))
 
+
+(defun escj2-verify-method-at-point ()
+  "Verify the method at point"
+  (interactive)
+  (let ((file-name (buffer-file-name (current-buffer)))
+	(method (car (jde-parse-get-method-at-point))))
+    (escj2-verify-method file-name 
+			 (car method) 
+			 (cdr method))))
+
+
+(defun escj2-verify-method (file-name class method)
+  "Verify a method"
+  (interactive)
+  (let ((files (list file-name))
+	(args (concat "-Routine " method)))
+    (escj2-run-verifier files args)))
+
+
+(defun escj2-run-verifier (files &rest args)
+  "Execute ESC/Java2 verifier
+
+   Run the verifier for the java source files in FILES, passing
+   it the arguments in ARGS.
+  "
+  (switch-to-buffer-other-window escj2-output-buffer-name)
+  (erase-buffer)
+  (goto-char (point-min))
+  (let ((program-args (append files args)))
+    (eval `(start-process "escj"
+		    escj2-output-buffer-name 
+		    escj2-command file-name 
+		    ,@program-args))))
